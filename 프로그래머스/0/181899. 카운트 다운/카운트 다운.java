bdaa52d87c0d75b7d835a9f78036b390271198f4
HuiGyun-kim <105576581+HuiGyun-kim@users.noreply.github.com>
2024-05-29 18:59:21 +0900

    [level 0] Title: 카운트 다운, Time: 0.21 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int start, int end_num) {
+        int[] answer = new int[start-end_num+1];
+        for(int i = 0; i <= answer.length-1; i++){
+            int result = start-i;
+            answer[i] = result;
+        }
+        System.out.println(Arrays.toString(answer));
+        return answer;
+    }
+}
