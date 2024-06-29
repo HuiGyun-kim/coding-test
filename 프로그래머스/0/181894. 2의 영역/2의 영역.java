386351e9c610aee4b29c5f10562c8c1b4e026368
HuiGyun-kim <105576581+HuiGyun-kim@users.noreply.github.com>
2024-06-29 16:54:53 +0900

    [level 0] Title: 2의 영역, Time: 1.18 ms, Memory: 95.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] arr) {
+        int firstIdx = -1;
+        int lastIdx = -1;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 2) {
+                if (firstIdx == -1) {
+                    firstIdx = i;
+                }
+                lastIdx = i;
+            }
+        }
+
+        if (firstIdx == -1) {
+            return new int[]{-1};
+        }
+
+        return Arrays.copyOfRange(arr, firstIdx, lastIdx + 1);
+    }
+}
